c03fe771994e8f2ee88cef3646aedbb4e518a5ef
LinKin-22 <shomnathcse22@gmail.com>
2019-10-12 23:56:13 +0600

    added child/parent components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   return (
+    // parent/ child components
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
