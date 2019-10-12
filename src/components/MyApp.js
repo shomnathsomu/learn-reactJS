2302c7cbcf953e9a2a1530bd1313780748673d43
LinKin-22 <shomnathcse22@gmail.com>
2019-10-12 23:59:47 +0600

    created components

---
+++ src/components/MyApp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Country List</h1>
+            <p>Some country names are following below:</p>
+            <ul>
+                <li>Bangladesh</li>
+                <li>Thailand</li>
+                <li>Japan</li>
+            </ul>
+            <p>There are some fruits list: </p>
+            <ol>
+                <li>Banana</li>
+                <li>Orange</li>
+                <li>Papaya</li>
+                <li>Apple</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MyApp;
