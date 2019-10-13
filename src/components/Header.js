1a925c38f2d99d91ea00400422e49bb4d0869443
LinKin-22 <shomnathcse22@gmail.com>
2019-10-13 16:36:27 +0600

    used header function from components

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return(
+      <nav>
+        <h1>Learn ReactJS</h1>
+        <ul>
+          <li>object 1</li>
+          <li>object 2</li>
+          <li>object 3</li>
+        </ul>
+      </nav>
+    )
+}
+
+export default Header;
