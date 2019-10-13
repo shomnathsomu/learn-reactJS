fee5df3d59ca299979fd55a838aaffb39bd22e32
LinKin-22 <shomnathcse22@gmail.com>
2019-10-13 16:33:56 +0600

    used through components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import Header from './Header';
-import Footer from './Footer';
-import MainContent from './MainContent';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+
 //import logo from './logo.svg';
 //import './App.css';
 
