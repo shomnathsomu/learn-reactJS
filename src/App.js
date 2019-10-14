0ad1b619dce0046d2630d498409031ca47058ff5
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-10-14 09:38:45 +0000

    added inline styles with styles property

--- src/App.js
+++ src/App.js
@@ -13,15 +13,29 @@ function App() {
   //const firstName = "Alice";
   //const secondName = "Doe";
 
-  const date = new Date();
+  //const date = new Date();
+  const date = new Date(2018, 6, 31, 22);
   const hours = date.getHours();
 
   let timeOfDay;
+  
+  const styles = {
+    backgroundColor: "#CDDC39",
+    fontSize: "30px"
+  };
 
-  if(hours < 12) timeOfDay = "morning";
-  else if (hours >= 12 && hours <= 18) timeOfDay = "afternoon";
-  else timeOfDay = "night";
-
+  if(hours < 12) {
+    timeOfDay = "morning";
+    styles.color = "lightGreen";
+  } 
+  else if (hours >= 12 && hours <= 18) {
+    timeOfDay = "afternoon";
+    styles.color = "blue"
+  } 
+  else {
+    timeOfDay = "night";
+    styles.color = "maroon";
+  }
 
   return (
     // parent/ child components
@@ -31,7 +45,8 @@ function App() {
     //   <Footer />
     // </div>
 
-    <h1>Good {timeOfDay}!</h1>
+    // // inline styles with styles property
+    <h1  style={styles}>Good {timeOfDay}!</h1>
   );
 }
 
