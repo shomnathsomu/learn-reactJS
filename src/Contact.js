889eaceef36a63c3fc3332697ec278e6af68e153
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-10-15 09:07:07 +0000

    Create Contact.js

---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ContactCard from './ContactCard';
+
+function Contact() {
+    return (
+        <div className="contacts">
+            <ContactCard contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}} />
+
+            <ContactCard contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2434", email: "fluff@me.com"}} />
+
+            <ContactCard contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300",phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}} />
+            
+            <ContactCard contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-6789", email: "thecat@hotmail.com"}} />
+        </div>
+    )
+}
+
+export default Contact;
