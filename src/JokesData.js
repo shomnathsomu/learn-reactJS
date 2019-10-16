ef27f200e6b43e8df5b714b71300c26eb367e22b
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-10-16 06:27:50 +0000

    Create JokesData.js

---
+++ src/JokesData.js
@@ -0,0 +1,33 @@
+const JokesData = [
+    {
+        id: 1,
+        punchLine: "It's hard to explain puns to klepmaniacs because they always take things literally."
+    },
+    {
+        id: 2,
+        question: "What's the best thing about Switzerland?",
+        punchLine: "I don't know, but the flag is a big plus!"
+    },
+    {
+        id: 3,
+        question: "Did you hear about the mathematician who's afraid of negetive numbers?",
+        punchLine: "He'll stop at nothing to avoid them!"
+    },
+    {
+        id: 4,
+        question: "Hear about the new restaurant called Karma?",
+        punchLine: "There's no menu: You get what you deserve."
+    },
+    {
+        id: 5,
+        question: "Did you hear about the actor who fell through floorboards?",
+        punchLine: "He was just going through a stage."
+    },
+    {
+        id: 6,
+        question: "Did you hear about the claustrobic astronaut?",
+        punchLine: "He just needed a little space."
+    }
+]
+
+export default JokesData;
