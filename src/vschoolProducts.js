d1b1eb0bd5d90487a865fc3066177802b889b271
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-10-16 10:26:22 +0000

    Create vschoolProducts.js

---
+++ src/vschoolProducts.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        id: "1",
+        name: "Pencil",
+        price: 1,
+        description: "Perfect for those who can't remember things! 5/5 highly recommend."
+    },
+    {
+        id: "2",
+        name: "Housing",
+        price: 0,
+        description: "Housing provided for out-of-state students or those who can't commute"
+    },
+    {
+        id: "3",
+        name: "Computer Rental",
+        price: 300,
+        description: "Don't have a computer? No problem!"
+    },
+    {
+        id: "4",
+        name: "Coffe",
+        price: 2,
+        description: "Wake up!"
+    },
+    {
+        id: "5",
+        name: "Snacks",
+        price: 0,
+        description: "Free snacks!"
+    }
+];
+
+export default products;
