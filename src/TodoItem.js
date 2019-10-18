55d40a21f95418d305deba822f5feb558db32f98
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-10-18 06:12:47 +0000

    Updated with props components

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function TodoItem() {
+function TodoItem(props) {
     return(
         <div className="todo-item">
-            <input type="checkbox"/>
-            <p>Text goes here..</p>
+            <input type="checkbox" checked={props.item.completed}/>
+            <p>{props.item.text}</p>
         </div>
     )
 }
