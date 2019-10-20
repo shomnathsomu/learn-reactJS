ff221fbf0cbf8ad17ca4a7736780417fd2c38006
LinKin-22 <shomnathcse22@gmail.com>
2019-10-20 23:48:39 +0600

    class-based components practice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
 // import MyApp from './components/MyApp';
 // import * as serviceWorker from './serviceWorker';
 
@@ -7,14 +7,56 @@ import ReactDOM from 'react-dom';
 // import Contact from './Contact';
 // import App from './App';
 
-import './index.css';
-import Todo from './Todo';
+//import './index.css';
+//import Todo from './Todo';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<h1>Hello World, Man!! </h1>, document.getElementById('root'));
 
+//======= Class based components practice======//\
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <Header username="Crimson" />
+                <Greeting />
+            </div>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return(
+            <header>
+                <p>Welcome, {this.props.username}!</p>
+            </header>
+        )
+    }
+}
+
+class Greeting extends Component {
+    render() {
+        const date = new Date();
+        const hours = date.getHours();
+    
+        let timeOfDay;
+        
+        if(hours < 12) {
+            timeOfDay = "morning";
+        } else if (hours >= 12 && hours <17) {
+            timeOfDay = "afternoon";
+        } else {
+            timeOfDay = "night";
+        }
+        return(
+            <h1>Good {timeOfDay} to you, Sir / Madam.</h1>
+        )
+    }
+}
+
 ReactDOM.render(
-    <Todo />
+    <App />
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
