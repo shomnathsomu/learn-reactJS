1d77affc5c897b09ecef6d671b190c61dbc37ad0
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-10-21 09:50:45 +0000

    Update with App.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 
 // import Joker from './Joker';
 // import Contact from './Contact';
-// import App from './App';
+import App from './App';
 
 //import './index.css';
 //import Todo from './Todo';
@@ -14,46 +14,47 @@ import ReactDOM from "react-dom";
 // ReactDOM.render(<h1>Hello World, Man!! </h1>, document.getElementById('root'));
 
 //======= Class based components practice======//\
-class App extends React.Component {
-    render() {
-        return(
-            <div>
-                <Header username="Crimson" />
-                <Greeting />
-            </div>
-        )
-    }
-}
+// class App extends React.Component {
+//     render() {
+//         return(
+//             <div>
+//                 <Header username="Crimson" />
+//                 <Greeting />
+//             </div>
+//         )
+//     }
+// }
 
-class Header extends React.Component {
-    render() {
-        return(
-            <header>
-                <p>Welcome, {this.props.username}!</p>
-            </header>
-        )
-    }
-}
+// class Header extends React.Component {
+//     render() {
+//         return(
+//             <header>
+//                 <p>Welcome, {this.props.username}!</p>
+//             </header>
+//         )
+//     }
+// }
 
-class Greeting extends Component {
-    render() {
-        const date = new Date();
-        const hours = date.getHours();
+// class Greeting extends Component {
+//     render() {
+//         const date = new Date();
+//         const hours = date.getHours();
     
-        let timeOfDay;
+//         let timeOfDay;
         
-        if(hours < 12) {
-            timeOfDay = "morning";
-        } else if (hours >= 12 && hours <17) {
-            timeOfDay = "afternoon";
-        } else {
-            timeOfDay = "night";
-        }
-        return(
-            <h1>Good {timeOfDay} to you, Sir / Madam.</h1>
-        )
-    }
-}
+//         if(hours < 12) {
+//             timeOfDay = "morning";
+//         } else if (hours >= 12 && hours <17) {
+//             timeOfDay = "afternoon";
+//         } else {
+//             timeOfDay = "night";
+//         }
+//         return(
+//             <h1>Good {timeOfDay} to you, Sir / Madam.</h1>
+//         )
+//     }
+// }
+//======= Class based components practice end======//\
 
 ReactDOM.render(
     <App />
