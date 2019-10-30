e0386164892cb816f711a3295569129456bd5081
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-10-30 09:12:30 +0000

    Update todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -10,11 +10,26 @@ class Todo extends React.Component {
         this.state = {
             todos: todosData
         }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if(todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
     }
 
     render() {
 
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
     
         return(
             <div className="todo-list">
