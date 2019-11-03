3de225798cf78fbb0c1d431deadbce812f5f8ac5
LinKin-22 <shomnathcse22@gmail.com>
2019-11-03 23:38:14 +0600

    Created some conditional statements

--- src/Conditional.js
+++ src/Conditional.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function Conditional(props) {
     return (
-        <h1> Temp </h1>
+        <div>
+            <h1>Navbar</h1>
+            
+            {props.isLoading ? <h1>Loading...</h1> : <h1> Some cool stuff about conditional rendering </h1>}
+
+            <h1>Footer</h1>
+        </div>
     )
 }
 
