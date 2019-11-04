ac03328cc2d54d2264d88578f42e36667f604134
LinKin-22 <shomnathcse22@gmail.com>
2019-11-04 23:52:36 +0600

    fetching data from an API

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import ReactDOM from "react-dom";
 //import App from './App';
 
 import './index.css';
-import Todo from './Todo';
-// import App from "./App";
+// import Todo from './Todo';
+import App from "./App";
 // import Events from "./Events";
 // import Count from './Count';
 
@@ -64,7 +64,7 @@ import Todo from './Todo';
 //======= Class based components practice end======//\
 
 ReactDOM.render(
-    <Todo />
+    <App />
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
