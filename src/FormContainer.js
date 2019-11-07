04ed566f90f34bb2d8d12b5a553f0d45f77bbd70
LinKin-22 <shomnathcse22@gmail.com>
2019-11-07 22:18:10 +0600

    Create FormContainer architecture

---
+++ src/FormContainer.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import FormComponent from './FormComponent';
+
+class Form extends Component {
+    constructor() {
+        super();
+        this.state = {
+            firstname: "",
+            lastname: "",
+            age: "",
+            gender: "",
+            destination: "",
+            isVegan: false,
+            isKosher: false,
+            isLactoseFree: false
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const {name, value, type, checked} = event.target;
+        type === "checkbox" ? this.setState({[name]: checked}) : this.setState({[name]: value})
+    }
+
+    handleSubmit(event) {
+        
+    }
+
+    render() {
+        return(
+            <FormComponent handleChange={this.handleChange} data={this.state}/>
+        )
+    }
+}
+
+export default Form;
